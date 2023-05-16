72978a3d46cf6e68f0cd5c5e3d55b656e549c5d3
admin <admin@DESKTOP-QJ2BC7G>
2023-05-16 18:26:41 +0530

    First commit

---
+++ MavenPractice/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+ 
+	@Test
+	void testCreateCustomer() {
+		System.out.println("Create Customer");
+	}
+}
